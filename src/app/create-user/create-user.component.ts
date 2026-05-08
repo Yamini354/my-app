b0789249a9aea9f10c4e4222ff4c1eb93cca330d
Yamini354 <yaminigolla519@gmail.com>
2026-05-08 16:38:32 +0530

    custom validator

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { capgeminiMail } from '../validators';
 
 @Component({
   selector: 'app-create-user',
@@ -12,7 +13,7 @@ export class CreateUserComponent {
     name: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
     age: new FormControl("", [Validators.required, Validators.min(0), Validators.max(100)]),
     phone: new FormControl("", [Validators.required, Validators.min(1000000000), Validators.max(9999999999)]),
-    email: new FormControl("",[Validators.required,Validators.email]),
+    email: new FormControl("",[Validators.required,Validators.email,capgeminiMail]),
     password: new FormControl("",[Validators.required,Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d)(?=[^!@#$%^&*]*[!@#$%^&*]).{8,}$/)]),
     address :new FormGroup({
       city:new FormControl("",[Validators.required]),
