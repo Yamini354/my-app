f00ce591dea9e40da7ae56c18088073db2c9a6ee
Yamini354 <yaminigolla519@gmail.com>
2026-05-13 12:24:39 +0530

    privious files

--- src/app/capital.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CapitalDirective } from './capital.directive';
-
-describe('CapitalDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CapitalDirective();
-    expect(directive).toBeTruthy();
-  });
-});
